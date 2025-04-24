2413cb39a87917c0874a48837b1818d5dbdc24b4
sayan4545 <sayanchatterjee3010@gmail.com>
2025-04-24 21:20:09 +0530

    basic synchronization and atomic  behaviour

---
+++ src/main/java/org/devSayan/synchronization/SynchronizationInitial.java
@@ -0,0 +1,32 @@
+package org.devSayan.synchronization;
+
+public class SynchronizationInitial {
+    private static int counter = 0;
+    public static void main(String[] args) {
+        
+        Thread one = new Thread(()->{
+            for(int i=0;i<10000;i++){
+                counter++;
+            }
+        });
+
+        Thread two = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter++;
+            }
+        });
+
+        one.start();
+        two.start();
+
+        try {
+            one.join();
+            two.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Final counter value: " + counter);
+    }
+    
+}
